176336b496f50720f1c53140a0931f6990d88366
wuyejuan <lavender78@sina.cn>
2022-06-09 21:13:28 +0800

    feat: import account for polkadot (#34)
    Co-authored-by: lavender78 <lindawu8134@gmail.com>

--- src/popup/pages/import-account/index.tsx
+++ src/popup/pages/import-account/index.tsx
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { HeaderWithBack } from '../../components/header';
-import { useAppSelector } from '../../../app/hooks';
-import { selectChain, selectNetwork} from '../../../reducer/network';
 import {Near} from '../../../api';
 import { useNavigate } from 'react-router-dom';
 import Content from '../../components/layout-content';
+import {useParams} from 'react-router-dom';
+import keyring from '@polkadot/ui-keyring';
 
 
 const ImportAccount = (props:any) => {
-    const networkId = useAppSelector(selectNetwork)
-    const chain = useAppSelector(selectChain(networkId));
     const [seeds, setSeeds] = useState('');
     const [errorText] = useState('');
     const navigator = useNavigate()
+    const {chain = 'near'} = useParams() as {chain: string};
     const handleImport = async () => {
         if(chain === 'near'){
             await Near.importAccount(seeds);
-            navigator('/dashboard');
         }
+        if(chain === 'appchains'){
+            keyring.addUri(seeds);
+        }
+        navigator('/dashboard');
     }
     const handleSetSeeds = (e:any) => {
         setSeeds(e.target.value);
