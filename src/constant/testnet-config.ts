56b5481e5793a8f1e463fc508afc9e7dde20944c
barry <421305864@qq.com>
2022-06-14 21:12:27 +0800

    balance

--- src/constant/testnet-config.ts
+++ src/constant/testnet-config.ts
@@ -16,30 +16,51 @@ const config = {
         netwoekId: 'testnet',
         nodeId: 'wss://gateway.testnet.octopus.network/fusotao/erc8ygm5qvmi2fw23ijpvzgpzzto47mi',
         name:'Fusotao',
+        symbol:"TAO",
         website: '',
         icon:"",
         tokenModule: 'token',
         tokenMethod: 'tokens',
         tokens:[
             {"code":0, decimal: 18, symbol: "TAO", "name":"TAO", "logo":"https://www.fusotao.org/share/tao.svg"},
-            {"code":1, decimal: 18, symbol:"USDT", "name":"USDT", "logo":"https://www.fusotao.org/share/usdt.svg"},
-            {"code":2, decimal: 18, symbol:"WETH", "name":"WETH", "logo":"https://www.fusotao.org/share/weth.svg"},
-            {"code":3, decimal: 18, symbol:"wNEAR", "name":"wNEAR", "logo":"https://www.fusotao.org/share/wnear.svg"}
+            {"code":1, decimal: 18, symbol: "USDT", "name":"USDT", "logo":"https://www.fusotao.org/share/usdt.svg"},
+            {"code":2, decimal: 18, symbol: "WETH", "name":"WETH", "logo":"https://www.fusotao.org/share/weth.svg"},
+            {"code":3, decimal: 18, symbol: "wNEAR", "name":"wNEAR", "logo":"https://www.fusotao.org/share/wnear.svg"}
         ]
     },
-    barnacle0918:{
-        nodeId: 'wss://gateway.testnet.octopus.network/barnacle0918/j8xz59egu4h8y814qnunm0cqfrq09lrw',
-        tokenModule: 'token',
-        tokenMethod: 'tokens',
-        tokens:[]
+    "barnacle0918":{
+        nodeId:"wss://gateway.testnet.octopus.network/barnacle0918/j8xz59egu4h8y814qnunm0cqfrq09lrw",
+        symbol:"BAR"
     },
-    myriad:{
-        nodeId: 'wss://gateway.testnet.octopus.network/myriad/8f543a1c219f14d83c0faedefdd5be6e',
-        tokenModule: 'token',
-        tokenMethod: 'tokens',
-        tokens:[]
+    "barnacle-evm":{
+        nodeId:"wss://gateway.testnet.octopus.network/barnacle-evm/wj1hhcverunusc35jifki19otd4od1n5",
+        symbol:"BARE"
     },
-
+    "discovol":{
+        nodeId:"wss://gateway.testnet.octopus.network/discovol/o4urcey87y4n1qimhfrad92gzs315z9h",
+        symbol:"DISC"
+    },
+    "myriad":{
+        nodeId:"wss://gateway.testnet.octopus.network/myriad/8f543a1c219f14d83c0faedefdd5be6e",
+        symbol:"MYRIA"
+    },
+    "debionetwork":{
+        nodeId:"wss://gateway.testnet.octopus.network/debionetwork/554976cbb180f676f188abe14d63ca24",
+        symbol:"DBIO"
+    },
+    "deip-test":{
+        nodeId:"wss://gateway.testnet.octopus.network/deip/46v4p8ss613olf92p2scmsjud68mhzrr",
+        symbol:"DEIP"
+    },
+    "atocha":{
+        nodeId:"ATO",
+        symbol:"wss://gateway.testnet.octopus.network/atocha/yevqd2d4jhm0dqakaj4hkbyjjfg6ukbu"
+    },
+    "uniqueone-appchain":{
+        nodeId:"wss://gateway.testnet.octopus.network/uniqueone/e83rnqoi4hr65cwx46a83u6a7a970dgq",
+        symbol:"UNET"
+    }
+    
 }
 
 export default config;
