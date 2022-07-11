544e603760a2b2a760bf13310697254d0747a882
barry <421305864@qq.com>
2022-07-11 18:34:29 +0800

    price

--- src/popup/pages/asset-page/index.tsx
+++ src/popup/pages/asset-page/index.tsx
@@ -63,7 +63,7 @@ const TotalAssets = (props:any)=>{
                   {chain === 'near' ? new Big(selectToken?.balance || 0).toFixed(4) : selectToken?.balance} {symbol.toUpperCase()}
                 </Typography>
                 <Typography variant="subtitle1" gutterBottom color="textSecondary">
-                  {selectToken?.balance} USD
+                  {selectToken?.price?selectToken?.price:"--"} USD
                 </Typography>
               </Card>
             </Grid>
