9ce3e0dcd0d0f3a0573a5f7fb862e18546e1e400
wuyejuan <lavender78@sina.cn>
2022-07-13 20:07:46 +0800

    feat: add app chain disconnect (#108)
    Co-authored-by: lavender78 <lindawu8134@gmail.com>

--- src/hooks/useAppChain.ts
+++ src/hooks/useAppChain.ts
@@ -1,18 +1,25 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import {appChain} from '../api';
 
 const useAppChain = (nodeId:string) => {
     const [api, setApi] = useState() as any;
+    const refreshAPI = useCallback(async (nodeId) => {
+        if(api){
+            await api.disconnect();
+            setApi(null);
+        }
+        const instance = appChain(nodeId);
+        await instance.isReady;
+        setApi(instance);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[nodeId])
     useEffect(() => {
         if(!nodeId){
-            return;
+            return ;
         }
-        (async() => {
-            const instance = appChain(nodeId);
-            await instance.isReady;
-            setApi(instance);
-        })()
-    },[nodeId])
+        refreshAPI(nodeId)
+    },[nodeId, refreshAPI])
+
 
     return api
 }
